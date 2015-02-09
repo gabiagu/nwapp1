0eda6ebd24125ab55b7c28bc497f1f6c4094e5fd
Gabriel Agu <agu.gabriel@gmail.com>
2015-02-09 22:12:53 +0100

    updated setup

--- nwapp/nwapp/routes/game.js
+++ nwapp/nwapp/routes/game.js
@@ -5,7 +5,7 @@ var start_data = generator.exported_words();
 /* GET users listing. */
 router.get('/game', function(req, res, next) {
   var start_data = generator.exported_words();
-  console.log(start_data);
+  //console.log(start_data);
   res.render('game', { title: 'game', start_data: start_data });
 });
 
--- nwapp/nwapp/w_generator.js
+++ nwapp/nwapp/w_generator.js
@@ -1,84 +1,104 @@
 module.exports = {
   exported_words: function() {
   // magic
-  //var items;
+    // initial vars
+    var fullDict = [],
+        seven_letter_words = [],
+        gameWordList = [],
+        validWordSelection = [],
+        masterword = '';
 
-    // placeholder for dictionary 
-    var items = [];
-    // object for mixed stuff
-    //===========================
-    var scrambledWords = [];
-    var finalSelection = [];
 
-    // load dictionary
-    //=============================
-    /*$.ajax({ 
-        type: 'GET', 
-        url: 'merged-eowl.txt', 
-        dataType:'text',
-        success: function (data) { 
-          items = data.split('\n');
-        }
-    });*/
-    var fs = require('fs');
-    fs.readFile('merged-eowl.txt', function(err, data) {
-        if(err) throw err;
-        items = data.toString().split("\n");
-    });
-
-    
-    //console.log('scrabledWords type of = '+typeof(scrambledWords));
-    // get masterword?
     //============================
-    var getWordsNew=function(masterword, callback){
+    var getWords=function(masterword, callback){
+
       var results={}
       var a,i,l;
       function nextLetter(a,l,key,used){
-          var i;
-          var j;
-          if(key.length==l){
-              return;
-          }
-          for(i=0;i<l;i++){
-              if(used.indexOf(""+i)<0){
-                  results[key+a[i]]="";
-                  nextLetter(a,l,key+a[i],used+i);
-              }
-          }
-       }
+        var i;
+        var j;
+        if(key.length==l){
+          return;
+        };
+        for(i=0;i<l;i++){
+          if(used.indexOf(""+i)<0){
+              results[key+a[i]]="";
+              nextLetter(a,l,key+a[i],used+i);
+          };
+        };
+      };
       a=masterword.split("");
-        l=a.length;
+      l=a.length;
       for (i = 0; i < a.length; i++) {
           results[a[i]] = "";
           nextLetter(a, l, a[i], "" + i)
-      }
+      };
+      // eliminate words shorter than 3 letters
       for(var key in results) {
         if (key.length > 2) {
-          scrambledWords.push(key);
+          gameWordList.push(key);
         };
         
-      }
-      for (var i = 0; i < scrambledWords.length; i++) {
-        var keytest = scrambledWords[i]+'\r';
-        console.log(items.length);
-        //console.log('indexof is '+items.indexOf(keytest));
-        if (items.indexOf(keytest) > -1) {
-          console.log(keytest+' is in items');
-          finalSelection.push(scrambledWords[i]);
-        }
-      }
-      callback(scrambledWords);
-
+      };
+      // validate against the full dictionary
+      for (var i = 0; i < gameWordList.length; i++) {
+        var keytest = gameWordList[i]+'\r';
+        if (fullDict.indexOf(keytest) > -1) {
+          validWordSelection.push(keytest);
+        };
+      };
+      // sort the set of valid words
+      validWordSelection.sort();
+      // send the final selection back as callback
+      callback(validWordSelection);
     };
 
-    // function to trigger anagrams
-    //========================
-    getWordsNew('father', function(word){
-      // returns scrabledWords
-      // console.log(scrambledWords);
-    });
-    //console.log(scrambledWords);
-    return scrambledWords;
+    // 3
+    reallyStartGettingData(getListOfSevenLetterWords);
 
+    // 1
+    // LOAD LIST OF 7 LETTER WORDS
+    //==============================================
+    function reallyStartGettingData(callback) {
+      var fs = require('fs');
+      fs.readFile('seven_letter_words.txt', function(err, data) {
+          if(err) throw err;
+          sevenLetterWordsList = data.toString().split(" ");
+          callback(sevenLetterWordsList);
+      });
+    };
+
+    // 2
+    function getListOfSevenLetterWords(sevenLetterWordsList){
+      
+      // 3
+      //=================================
+      startGettingData(loadFullDict);
+      //console.log(sevenLetterWordsList.length);
+      //console.log(typeof(sevenLetterWordsList));
+      // 1 LOAD FULL DICTIONARY
+      //===================================
+      function startGettingData(callback) {
+        var fs = require('fs');
+        fs.readFile('eowl_dict.txt', function(err, data) {
+            if(err) throw err;
+            fullDict = data.toString().split("\n");
+            callback(fullDict);
+        });
+      };
+
+      // 2
+      //===================================
+      function loadFullDict(fullDict) {
+        
+        var masterword = sevenLetterWordsList[Math.floor(Math.random()*sevenLetterWordsList.length)];
+        //console.log(masterword);
+        getWords(masterword, function(word){
+          // will return validWordSelection - array with the set of words
+        });
+      };
+    };
+
+    return validWordSelection;
   }
 };
